99ebe1b23339947c1690938b009b9d54bb4e3b37
ada-bos <adrianaboscolo1@gmail.com>
2021-12-16 15:14:46 +0100

    fixed error handling for routes, added /health route and password hashing

--- auth/src/user_handling/routes.rs
+++ auth/src/user_handling/routes.rs
@@ -1,23 +1,25 @@
 use crate::Pool;
 use super::handlers;
 use super::models::NewUser;
-use actix_web::http::StatusCode;
-use actix_web::{post,get, web,HttpRequest, HttpResponse};
+use actix_web::{post,get, web,HttpRequest, HttpResponse, Error};
+
 
 #[post("/login")]
 async fn login(pool: web::Data<Pool>, user: web::Json<NewUser>) -> HttpResponse {
     let res = handlers::login(pool, user);
     match res {
         Ok(_) => HttpResponse::Ok().json(res.unwrap()),
-        Err(_) => HttpResponse::Ok()
-            .status(StatusCode::from_u16(401).unwrap())
-            .json(res.unwrap_err()),
+        Err(_) => HttpResponse::BadRequest().json(res.unwrap_err()),
     }
 }
 
 #[post("/register")]
 async fn register(pool: web::Data<Pool>, user: web::Json<NewUser>) -> HttpResponse {
-    HttpResponse::Ok().json(handlers::add_user(pool, user))
+    let res = handlers::add_user(pool, user);
+    match res {
+        Ok(_) => HttpResponse::Ok().json(res.unwrap()),
+        Err(_) => HttpResponse::BadRequest().json(res.unwrap_err()),
+    }
 }
 
 #[get("/authenticate")]
@@ -27,12 +29,22 @@ async fn authenticate(_req: HttpRequest) -> HttpResponse {
     let token = _split[1].trim();
     match handlers::auth_function(token) {
         Ok(result) => HttpResponse::Ok().json(result),
-        Err(err) => HttpResponse::Ok().json(err),
+        Err(err) => HttpResponse::Unauthorized().json(err),
     }
 }
+
+#[get("/health")]
+async fn health(pool: web::Data<Pool>) -> Result<HttpResponse, Error>{
+    Ok(web::block(move || handlers::check_health(pool))
+        .await
+        .map(|_| HttpResponse::Ok().json("Connection intact"))
+        .map_err(|_| HttpResponse::InternalServerError())?)
+}
+
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(login);
     cfg.service(register);
     cfg.service(authenticate);
+    cfg.service(health);
 }
